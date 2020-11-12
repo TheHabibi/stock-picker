5dcce7ecb6c8d89c4c8a7bd5001868a5f5cfcd31
TheHabibi <celebialieren@gmail.com>
2020-11-13 00:04:19 +0300

    finished

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,20 +1,18 @@
 def stock_picker(arr)
-
-    temp = arr.sort
-
-    if arr.index(arr.max()) - arr.index(arr.min()) > 0
-        new_arr = [arr.index(arr.min()), arr.index(arr.max())]
-        puts new_arr
-    else
-        temp.pop
-        temp.shift
-        puts temp
-        new_arr = [arr.index(temp.min()), arr.index(temp.max())]
-        puts new_arr
+    profit = 0
+    buy = 0
+    sell = 0
+    arr.each_with_index do |price, index|
+        for i in 1..(arr.length-1)-index
+            if arr[index+i]-price > profit
+                profit = arr[index+i] - price
+                buy = index
+                sell = index+i
+            end
+        end 
     end
-
+    return [buy, sell] 
 end
-
 
 
 stock_picker([17,3,6,9,15,8,6,1,10])
